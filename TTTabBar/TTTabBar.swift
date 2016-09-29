b10a0eaae1100592c4c571be5bc958c71f9e584d
Stephane <Lmaix>
2016-09-29 14:23:58 +0200

    swift 3

--- TTTabBar/TTTabBar.swift
+++ TTTabBar/TTTabBar.swift
@@ -1,316 +1,98 @@
 //
-//  TTTabBar.swift
-//  Tutton
+//  TabBar.swift
+//  Octoly
 //
-//  Created by Eduardo Iglesias on 3/6/15.
-//  Copyright (c) 2015 Tutton. All rights reserved.
+//  Created by Adrien on 08/03/16.
+//  Copyright © 2016 Octoly. All rights reserved.
 //
 
 import UIKit
+import TTTabBar
 
-public class TTTabBar: UIViewController {
+class TabBar: TTTabBar {
     
-    private var detailView: UIView! //View that will show controllers
-    public var activeTabBar: TTTabBarItem? //Active showing view Controller
-    private var activeView: UIView? //Active showing view Controller
-    private var tabBarView: UIView! //View of tabBar
-    private var contentTabBarView : UIView! //Content, where background is render
+    var tabbars:NSMutableArray = NSMutableArray()
     
-    private var tabBarHidden = false
-    
-    //TabBar Items, which include VC
-    public var tabBarItems: [TTTabBarItem] = []
-    
-    private let defaultTabBarHeight: CGFloat = 44
-    private var initialTabBarHeight: CGFloat = 0
-    
-    //TabBar Custom
-    public var tabBarHeight: CGFloat = 0 //Height of the TabBar, if a TTTabBarItem is bigger, will be over the tabBar
-    public var defaultTabBarItem: TTTabBarItem!
-    public var spaceBetweenTabs: CGFloat = 5
-    public var tabBackgroundColor = UIColor.white
-    
-    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
-        return CGRect(x: x, y: y, width: width, height: height)
-    }
-    
-    override public func viewDidLoad() {
+    override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-        //Defaults
-        if (tabBarHeight == 0) {
-            tabBarHeight = defaultTabBarHeight
-        }
+        super.view.backgroundColor = UIColor.clear
         
-        initialTabBarHeight = tabBarHeight
+        NotificationCenter.default.addObserver(self, selector: #selector(self.updateBubble(_:)), name: NSNotification.Name(rawValue: "updateBubble"), object: nil)
         
+        super.tabBarHeight = MENU_HEIGHT
+        super.spaceBetweenTabs = 0
         
-        //Create the detailView
-        detailView = UIView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height-tabBarHeight))
-        detailView.backgroundColor = UIColor.clear
+        let nav1 = UINavigationController()
+        nav1.viewControllers = [ProductsController()]
         
-        //Creat TabBar view
-        tabBarView = UIView(frame: CGRectMake(0, self.view.frame.height-tabBarHeight, self.view.frame.width, tabBarHeight))
-        contentTabBarView = UIView(frame: CGRectMake(0, 0, self.view.frame.width, tabBarHeight))
+        let item1 = TabBarItem(viewController:nav1)
+        item1.image = UIImage(named: "menu-home")
+        item1.setItem("home")
         
-        //defaults
-        tabBarView.backgroundColor = UIColor.clear
+        let item2 = TabBarItem(viewController:OrdersController())
+        item2.image = UIImage(named: "menu-orders")
+        item2.setItem("orders")
         
-        //Add subviews to mainView
-        tabBarView.addSubview(contentTabBarView)
-        self.view.addSubview(detailView)
-        self.view.addSubview(tabBarView)
+        let nav3 = UINavigationController()
+        nav3.viewControllers = [InboxController()]
+        
+        let item3 = TabBarItem(viewController:nav3)
+        item3.image = UIImage(named: "menu-inbox")
+        item3.setItem("inbox")
+        
+        let nav4 = UINavigationController()
+        nav4.viewControllers = [BlogController()]
+        
+        let item4 = TabBarItem(viewController:nav4)
+        item4.image = UIImage(named: "menu-magazine")
+        item4.setItem("magazine")
+        
+        let nav5 = UINavigationController()
+        nav5.viewControllers = [SettingsController()]
+        
+        let item5 = TabBarItem(viewController:nav5)
+        item5.image = UIImage(named: "menu-account")
+        item5.setItem("account")
+        
+        self.tabBackgroundColor = MENU_BACKGROUND_COLOR
+        self.tabBarItems = [item1, item2, item3, item4, item5]
+        self.updateTabBarView()
+        self.updateItemsWidth()
     }
     
-    //Modify tabbar with custom options
-    public func updateTabBarView() {
+    override func ttTabBar(tabBar: TTTabBar, shouldChangeTab tabBarItem: TTTabBarItem) -> Bool {
         
-        //Update the detailView
-        detailView.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height-tabBarHeight)
+        let tb = tabBar as! TabBar
+        let tbitem = tabBarItem as! TabBarItem
         
-        //Update TabBar view
-        tabBarView.frame = CGRectMake(0, self.view.frame.height-tabBarHeight, self.view.frame.width, tabBarHeight)
-        
-        //Verify that has defaultTabBar
-        if let tabBar = defaultTabBarItem {
-            
-        }else{
-            if tabBarItems.count > 0 {
-                defaultTabBarItem = tabBarItems[0]
-            }
+        for item in tb.tabBarItems {
+            let i = item as! TabBarItem
+            i.unselectItem()
         }
         
-        self.updateBackgroundColor(color: tabBackgroundColor)
-        self.updateTabBarHeight()
-        self.renderButtons()
-    }
-    
-    private func updateBackgroundColor (color: UIColor) {
-        contentTabBarView.backgroundColor = color
-    }
-    
-    public func updateTabBarHeight() {
-        for tabBarItem in tabBarItems {
-            
-            //if one button is greather default. Set the tabBarView greather
-            if tabBarItem.frame.height + tabBarItem.offsetBottom > tabBarView.frame.height {
-                tabBarHeight = tabBarItem.frame.height + tabBarItem.offsetBottom
-                tabBarView.frame = CGRectMake(0, self.view.frame.height-tabBarHeight, self.view.frame.width, tabBarHeight)
-                contentTabBarView.frame = CGRectMake(0, tabBarHeight-initialTabBarHeight, self.view.frame.width, initialTabBarHeight)
-                
-                
-            }
-        }
-    }
-    
-    private func renderButtons() {
-        
-        //Get all TabBarItems and divide the width between
-        let widthPerButton = (self.view.frame.width - spaceBetweenTabs*CGFloat(tabBarItems.count))/CGFloat(tabBarItems.count)
-        
-        //Add subview buttons to tabBar
-        var tempX = spaceBetweenTabs //Start position of buttons, logical position
-        for tabBarItem in tabBarItems {
-            //Set images if has
-            if let image = tabBarItem.image {
-                tabBarItem.setImage(image, for: UIControlState.normal)
-            }
-            
-            if defaultTabBarItem == tabBarItem {
-                //Load the default VC
-                self.loadViewControllerFrom(tabBarItem: tabBarItem)
-            }
-            
-            //Call action on touchUpInside
-            tabBarItem.addTarget(self, action: "tabBarItemClicked:", for: UIControlEvents.touchUpInside)
-            
-            //if the height of the tabBarItem, is default (40), change to tabBarView height
-            var newHeight = tabBarItem.frame.height
-            if tabBarItem.frame.height == defaultTabBarHeight {
-                newHeight = defaultTabBarHeight
-            }
-            
-            //if width of tanBarItem is not 0, set the custom size
-            var customWidth = widthPerButton
-            var positionX = tempX
-            if tabBarItem.frame.width > 0 {
-                customWidth = tabBarItem.frame.width
-                
-                //New position of X
-                //Center button on distance between buttons + custom width
-                positionX = tempX + widthPerButton/2 - customWidth/2
-            }
-            
-            //Modify frame of TabBarItems
-            //- tabBarItem.offsetBottom
-            let off = (tabBarView.frame.height - tabBarItem.frame.height) - tabBarItem.offsetBottom
-            tabBarItem.frame = CGRectMake(positionX, tabBarItem.offsetY + off , customWidth, newHeight)
-            tempX += widthPerButton+spaceBetweenTabs
-            
-            tabBarView.addSubview(tabBarItem)
-        }
-    }
-    
-    public func tabBarItemClicked(sender: AnyObject) {
-        if let tabBar = sender as? TTTabBarItem {
-            self.loadViewControllerFrom(tabBarItem: tabBar)
-        }
-    }
-    
-    public func loadViewControllerFrom(tabBarItem: TTTabBarItem?) {
-        if let item = tabBarItem {
-            if !self.ttTabBar(tabBar: self, shouldChangeTab: item) {
-                return
-            }
-            
-            //if users click on the same tab that is active, return
-            if let tabBar = activeTabBar {
-                if item == activeTabBar {
-                    return
-                }
-            }
-        }
-        
-        //Change image to the old tabBarItem to no selected
-        if let tabBar = activeTabBar {
-            
-            self.ttTabBar(tabBar: self, tabWillDisappear: tabBar)
-            if let image = tabBar.image {
-                tabBar.setImage(image, for: UIControlState.normal)
-            }
-            
-            //Remove actual View
-            if let vc = tabBar.viewController {
-                vc.view.removeFromSuperview()
-            }
-            
-            self.ttTabBar(tabBar: self, tabDidDisappear: tabBar)
-        }
-        
-        if let view = activeView {
-            view.removeFromSuperview()
-        }
-        
-        if let item = tabBarItem {
-            //Change image to the new tabBarItem to selected
-            if let image = item.selectedImage {
-                item.setImage(image, for: UIControlState.normal)
-            }
-            
-            
-            //add VC to detailView
-            if let vc = item.viewController {
-                self.ttTabBar(tabBar: self, tabWillAppear: item)
-                //set active bar
-                activeTabBar = item
-                
-                vc.view.frame = self.detailView.bounds
-                detailView.addSubview(vc.view)
-                self.addChildViewController(vc)
-                vc.didMove(toParentViewController: self)
-                
-                self.ttTabBar(tabBar: self, tabDidAppear: item)
-            }
-        }
-    }
-    
-    //IF you need to load an external view controller, that is not on the tab menu
-    public func loadViewController(vc: UIViewController) {
-        //Change image to the old tabBarItem to no selected
-        if let tabBar = activeTabBar {
-            self.ttTabBar(tabBar: self, tabWillDisappear: tabBar)
-            if let image = tabBar.image {
-                tabBar.setImage(image, for: UIControlState.normal)
-            }
-            
-            //Remove actual View
-            if let vc = tabBar.viewController {
-                vc.view.removeFromSuperview()
-            }
-            self.ttTabBar(tabBar: self, tabDidDisappear: tabBar)
-        }
-        
-        if let view = activeView {
-            view.removeFromSuperview()
-        }
-        
-        //add VC to detailView
-        //set active bar
-        activeTabBar = nil
-        activeView = vc.view
-        
-        vc.view.frame = self.detailView.bounds
-        detailView.addSubview(vc.view)
-        self.addChildViewController(vc)
-        vc.didMove(toParentViewController: self)
-    }
-    
-    // MARK: - Get Functions
-    public func getActualController() -> UIViewController? {
-        if let activeTab = activeTabBar {
-            return activeTab.viewController
-        }
-        return nil
-    }
-    
-    //MARK: hide/show tabBar
-    public func hideTabBar(animated: Bool) {
-        if !tabBarHidden  {
-            UIView.animate(withDuration: 0.2) {
-                self.tabBarView.frame.origin.y += self.tabBarView.frame.width
-                self.detailView.frame.size.height = self.view.frame.height
-            }
-            tabBarHidden = true
-        }
-    }
-    
-    public func showTabBar(animated: Bool) {
-        if tabBarHidden  {
-            UIView.animate(withDuration: 0.2) {
-                self.tabBarView.frame.origin.y -= self.tabBarView.frame.width
-                self.detailView.frame.size.height -= self.contentTabBarView.frame.height
-            }
-            
-            tabBarHidden = false
-        }
-        
-    }
-    
-    // MARK: - Active TabBar
-    public func isTabSelected(tab: TTTabBarItem) {
-        if activeTabBar == tab {
-            
-        }
-    }
-    
-    
-    //MARK: overridable Func
-    public func ttTabBar(tabBar: TTTabBar, shouldChangeTab tabBarItem: TTTabBarItem) -> Bool {
-        if tabBarItem.isButton {
-            self.ttTabBar(tabBar: tabBar, buttonHasBeenClicked: tabBarItem)
-            return false
-        }
-        
+        tbitem.selectItem()
         return true
     }
     
-    public func ttTabBar(tabBar: TTTabBar, tabWillDisappear tabBarItem: TTTabBarItem) {
+    func updateItemsWidth() -> Void {
         
+        for item in self.tabBarItems {
+            let i = item as! TabBarItem
+            i.updateWidth()
+        }
     }
     
-    public func ttTabBar(tabBar: TTTabBar, tabDidDisappear tabBarItem: TTTabBarItem) {
+    func updateBubble(_ notification:Foundation.Notification) -> Void {
         
-    }
-    
-    public func ttTabBar(tabBar: TTTabBar, tabWillAppear tabBarItem: TTTabBarItem) {
+        let userInfo:Dictionary<String, String> = (notification as NSNotification).userInfo as! Dictionary<String, String>
         
-    }
-    
-    public func ttTabBar(tabBar: TTTabBar, tabDidAppear tabBarItem: TTTabBarItem) {
-        
-    }
-    
-    public func ttTabBar(tabBar: TTTabBar, buttonHasBeenClicked tabBarItem: TTTabBarItem) {
-        
+        for item in self.tabBarItems {
+            let i = item as! TabBarItem
+            if (i.name == userInfo["item"]) {
+                i.setBubble(i.name!, bubble: Int(userInfo["bubble"]!)!)
+            }
+        }
     }
 }
--- TTTabBar/TTTabBarItem.swift
+++ TTTabBar/TTTabBarItem.swift
@@ -1,46 +1,85 @@
 //
-//  TTTabBarItem.swift
-//  Tutton
+//  TabBarItem.swift
+//  Octoly
 //
-//  Created by Eduardo Iglesias on 3/6/15.
-//  Copyright (c) 2015 CreApps. All rights reserved.
+//  Created by Adrien on 08/03/16.
+//  Copyright © 2016 Octoly. All rights reserved.
 //
 
 import UIKit
+import TTTabBar
 
-public class TTTabBarItem: UIButton {
+class TabBarItem: TTTabBarItem {
     
-    public var offsetY: CGFloat = 0 //Offset from Top Y
-    public var offsetBottom: CGFloat = 0 //Offset from Bottom, like margin
+    var line:UIView?
+    var name:String?
+    var label:UILabel?
     
-    public var image: UIImage?
-    public var selectedImage: UIImage?
-    public var viewController: UIViewController?
-    
-    //If is a button, set to yes
-    public var isButton = false
-    
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    public override func draw(_ rect: CGRect) {
-        // Drawing code
-        super.draw(rect)
+    override init(viewController: UIViewController) {
+        super.init(viewController: viewController)
+        
+        super.offsetY = 0
+        super.offsetBottom = 0
+        
+        self.backgroundColor = MENU_BACKGROUND_COLOR
+        
+        self.line = UIView()
+        self.line?.backgroundColor = HIGLIGHT_BACKGROUND_COLOR
+        self.addSubview(self.line!)
     }
     
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder:aDecoder)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    //Init with default size
-    public init(image: UIImage?, selected: UIImage?) {
-        super.init(frame: CGRect(x:0, y:0, width:0, height: 44))
-        self.setImage(image, for: UIControlState.normal)
-        self.image = image
-        self.selectedImage = selected
+    internal func unselectItem() {
+        self.backgroundColor = MENU_BACKGROUND_COLOR
+        self.line?.alpha = 0
     }
     
-    public init(viewController: UIViewController) {
-        super.init(frame: CGRect(x:0, y:0, width:0, height: 44))
-        self.viewController = viewController
+    internal func selectItem() {
+        self.backgroundColor = MENU_ACTIVE_BACKGROUND_COLOR
+        self.line?.alpha = 1
+    }
+    
+    internal func updateWidth() {
+        self.frame = CGRect(x: self.frame.origin.x-1, y: 0, width: self.frame.width+2, height: MENU_HEIGHT)
+        self.line?.frame = CGRect(x: 0, y: self.frame.height - MENU_LINE_HEIGHT, width: self.frame.width, height: MENU_LINE_HEIGHT)
+        self.label?.frame = CGRect(x: 40, y: 8, width: 16, height: 16)
+    }
+    
+    func hideNew() {
+        self.label?.alpha = 0
+    }
+    
+    func setItem(_ name:String) {
+        
+        self.name = name
+        
+        if (self.name == "inbox") {
+            
+            label = UILabel()
+            label?.textAlignment = .center
+            label?.textColor = WHITE
+            label?.font = UIFont.boldSystemFont(ofSize: 9)
+            label?.backgroundColor = HIGLIGHT_BACKGROUND_COLOR
+            label?.layer.cornerRadius = 6
+            label?.layer.masksToBounds = true
+            label?.alpha = 0
+            self.addSubview(self.label!)
+        }
+    }
+    
+    func setBubble(_ name: String, bubble:Int) {
+        
+        if (self.name == name) {
+            
+            if (bubble > 0) {
+                label?.text = String(format:"%d",bubble)
+                label?.alpha = 1
+            } else {
+                label?.alpha = 0
+            }
+        }
     }
 }
